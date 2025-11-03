fe00ef3451188674eaad7ead73c8eade9ca9ae19
Ben Osborn <benosborntech@gmail.com>
2025-11-03 20:34:21 +1100

    feat: s3 kv store

--- go.mod
+++ go.mod
@@ -7,12 +7,18 @@ toolchain go1.24.9
 require (
 	github.com/aws/aws-sdk-go-v2 v1.39.5
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.52.3
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.89.1
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.2 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.12 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.12 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.11.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.12 // indirect
 	github.com/aws/smithy-go v1.23.1 // indirect
 )
---
+++ pkg/kvstore/s3.go
@@ -0,0 +1,49 @@
+package kvstore
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3Client struct {
+	s3     *s3.Client
+	bucket string
+}
+
+func NewS3Client(s3c *s3.Client, bucket string) Client {
+	return &S3Client{s3: s3c, bucket: bucket}
+}
+
+func (s *S3Client) Get(ctx context.Context, key string) (string, error) {
+	out, err := s.s3.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to get object, err=%w", err)
+	}
+	defer out.Body.Close()
+	b, err := io.ReadAll(out.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read body, err=%w", err)
+	}
+	return string(b), nil
+}
+
+func (s *S3Client) Put(ctx context.Context, key string, value string) error {
+	b := []byte(value)
+	_, err := s.s3.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(b),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to put object, err=%w", err)
+	}
+	return nil
+}
